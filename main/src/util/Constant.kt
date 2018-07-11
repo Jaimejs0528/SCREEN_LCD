9cfe9b34e231ca75c2618afbb807fb0286c533fb
Jaime Jimenez Serrano <jaime.jimenezs@udea.edu.co>
2018-07-10 19:37:41 -0500

    refactor an error messages

--- main/src/util/Constant.kt
+++ main/src/util/Constant.kt
@@ -17,10 +17,10 @@ class Constant {
 
         //Messages
         val NOT_SEPARATOR_SIGN_MESSAGE: String = "La entrada no posee el caracter separador:"
-        val NOT_IS_A_NUMBER_MESSAGE: String = "La siguiente cadena, NO es un número:"
+        val NOT_IS_A_NUMBER_MESSAGE: String = "Numero ingresado invalido:"
         val OUT_SIZE_RANGE_MESSAGE: String = "El tamaño debe estar entre $MIN_VALUE_SIZE y $MAX_VALUE_SIZE:"
         val OUT_SPACE_RANGE_MESSAGE: String = "El valor debe estar entre $MIN_VALUE_SPACE y $MAX_VALUE_SPACE:"
-        val TOO_SHORT: String = "La entrada ingresada no es valida:"
+        val TOO_SHORT: String = "La entrada ingresada es demasiado corta:"
         val SPACE_INPUT_MESSAGE: String = "Ingresa el espacio entre digitos (0-5):"
         val SCREEN_INPUT_MESSAGE: String = "Entrada: "
         val NOT_SCREEN_FOUND_MESSAGE: String = "La pantalla no existe"
